767f7faf576b059f1066c0c1f6abdca272cf4622
Moooh2020 <mohmohalhady@gmail.com>
2021-12-09 15:10:58 +0300

    Add project files.

---
+++ Web5/Startup.cs
@@ -0,0 +1,48 @@
+using Infrastructure;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Web5
+{
+    public class Startup
+    {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddDbContext<DataContext>(Options =>
+            {
+                Options.UseSqlServer(configuration.GetConnectionString("MyPortfolioDB"));
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+            "defaultRoute",
+                "{controller=Home}/{action=index}/{id?}"
+                );
+            });
+        }
+    }
+}
